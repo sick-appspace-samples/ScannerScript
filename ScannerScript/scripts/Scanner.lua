c3cb8769e107894b88d31c750bbbb66264cbe076
Florian Roser <Florian.Roser@sick.de>
2019-05-02 17:39:10 +0200

    Initial commit

---
+++ ScannerScript/scripts/Scanner.lua
@@ -0,0 +1,43 @@
+--[[----------------------------------------------------------------------------
+
+  Application Name: ScannerScript
+
+  Description:
+  Retrieving scans from remote device and view as point cloud using Lua script.
+
+  This sample retrieves scan data from a connected scanner device. To run this
+  sample, the IP address in the script has to match the device.
+  Scans can be viewed in the PointCloud gViewer on the webpage.
+
+------------------------------------------------------------------------------]]
+--Start of Global Scope---------------------------------------------------------
+
+-- luacheck: globals gScanProvider gViewer gTransformer gHandleOnNewScan
+
+-- Provide scans from device
+gScanProvider = Scan.Provider.RemoteScanner.create()
+Scan.Provider.RemoteScanner.setIPAddress(gScanProvider, '192.168.0.1')
+-- Register callback, starts providing automatically
+Scan.Provider.RemoteScanner.register( gScanProvider, 'OnNewScan', 'gHandleOnNewScan' )
+-- Create a viewer instance
+gViewer = View.create()
+gViewer:setID('viewer3D')
+
+-----------------------------------------------------------------------------------------------
+-- Create a transform instance to convert the Scan to a PointCloud
+gTransformer = Scan.Transform.create()
+
+--End of Global Scope-----------------------------------------------------------
+
+--Start of Function and Event Scope---------------------------------------------
+
+-- Callback function to process new scans
+function gHandleOnNewScan(scan)
+  -- View the scan on the Scan viewer on the webpage.
+  View.view(gViewer, scan)
+  -- Transform to PointCloud to view in the PointCloud viewer on the webpage
+  local pointCloud = Scan.Transform.transformToPointCloud(gTransformer, scan)
+  View.view(gViewer, pointCloud)
+end
+
+--End of Function and Event Scope------------------------------------------------
---
+++ readme.md
@@ -0,0 +1,10 @@
+## ScannerScript
+Retrieving scans from remote device and view as point cloud using Lua script.
+### Description
+This sample retrieves scan data from a connected scanner device. 
+### How to Run
+To run this sample, the IP address in the script has to match the device.
+Scans can be viewed in the PointCloud viewer on the webpage
+
+### Topics
+Remote-Device, Scan, Point-Cloud, Acquisition, Sample, SICK-AppSpace
